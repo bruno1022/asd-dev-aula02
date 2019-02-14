0f572c11eedf6e8d5028cb5b7546cd524068e64c
Felipe Antônio Dos Santos Silva <798311@sga.pucminas.br>
2019-02-14 21:08:39 -0200

    Estrutura básica de metodos

--- src/main/java/com/empresa/util/PokerUtil.java
+++ src/main/java/com/empresa/util/PokerUtil.java
@@ -1,5 +1,28 @@
 package com.empresa.util;
 
+import com.empresa.enums.JogadorEnum;
+import com.empresa.enums.TipoMao;
+
 public class PokerUtil {
+	
+	/**
+	 * Recebe as cartas de 1 jogador
+	 * 
+	 * @param cartasDaMao - cada posição do array representa uma carta. Ex.: "5C" (5 de Copas)
+	 * @return Tipo da Mao (Enum)
+	 */
+	public TipoMao identificarTipoMao(String[] cartasDaMao) {
+		return null; //TODO Implementar
+	}
+	
+	/**
+	 * Compara os tipos das mãos para definir o vencedor. Caso sejam do mesmo tipo, retorna empate.
+	 * @return
+	 */
+	public JogadorEnum compararTipoMao(TipoMao maoJogador1, TipoMao maoJogador2) {
+		return null; //TODO Implementar
+	}
+	
+	
 
 }
